2766aa033283f2dbe09cf116ce8239fe5da7b59c
rupali <36826017+rupalimo@users.noreply.github.com>
2022-04-15 16:28:29 -0400

    update to cursor, remove font-family

--- project-files/03-Lishan-Marton/script.js
+++ project-files/03-Lishan-Marton/script.js
@@ -1,28 +1,16 @@
 // option for Lishan & Marton, edit so that the mouse has a tail in background but disappears over focus area
 
-let font,
-  fontsize = 12;
-
-function preload() {
-  // Ensure the .ttf or .otf font stored in the assets directory
-  // is loaded before setup() and draw() are called
-  font = loadFont("Cousine-Regular.ttf");
-}
-
 function setup() {
   createCanvas(windowWidth, windowHeight);
 
-  textFont(font);
-  textSize(fontsize);
-  textAlign(CENTER, CENTER);
 }
 
 function draw() {
   background(255);
 
-  // text
+
   fill(0);
-  text("i do not want to tell this story", mouseX, mouseY);
+  text("(i do not want to tell this story)", mouseX -60, mouseY);
 
   // need to figure out how to add borderbox:
 
